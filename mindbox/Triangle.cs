57139f8d84ee3fb065dbfdba5e76be33d42c8b94
Иван Иванов <bolotin99@inbox.ru>
2020-11-11 20:36:39 +0300

    Implementation Triangle

---
+++ mindbox/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace mindbox
+{
+  class Triangle : IShape
+  {
+    private readonly double _A;
+    private readonly double _B;
+    private readonly double _C;
+
+    public Triangle(double a, double b, double c)
+    {
+      if (a <= 0 || b <= 0 || c <= 0)
+        throw new ArgumentException("Side must be more then zero!");
+
+      _A = a;
+      _B = b;
+      _C = c;
+    }
+
+    public double Area =>
+       compArea();
+
+    private double compArea()
+    {
+      double p = (_A + _B + _C) / 2;
+      return Math.Sqrt(p * (p - _A) * (p - _B) * (p - _C));
+    }
+
+    public bool IsRight() =>
+      isRight();
+
+    private bool isRight()
+    {
+      double a = _A * _A;
+      double b = _B * _B;
+      double c = _C * _C;
+      return
+        a + b - c < double.Epsilon ||
+        a + c - b < double.Epsilon ||
+        c + b - a < double.Epsilon;
+    }
+  }
+}
