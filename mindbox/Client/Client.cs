0c4d87820107c589c038a74cb180bca9b6f017d6
Иван Иванов <bolotin99@inbox.ru>
2020-11-11 21:37:28 +0300

    Implement Client Add methods for work with Shapes

---
+++ mindbox/Client/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using mindbox.Lib;
+
+namespace mindbox.Client
+{
+  class Client
+  {
+    public double CalculateTriangleArea(double a, double b, double c) =>
+      new Triangle(a, b, c).Area;
+
+    public double CalculateCircleArea(double r) =>
+      new Circle(r).Area;
+
+    public double CalculateShapeArea(IShape shape) =>
+      shape.Area;
+
+    public bool IsRightTriangle(double a, double b, double c) =>
+      new Triangle(a, b, c).IsRight();
+    
+  }
+}
